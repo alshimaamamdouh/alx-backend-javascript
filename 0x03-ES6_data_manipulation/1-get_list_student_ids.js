e6e873f1a5e1b51b4e1c9035f4005f7fe7b227f5
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-06-04 23:42:26 +0000

    task0

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,7 @@
-const getListStudentIds = (students)=>{
-	if (!Array.isArray(students)) {
-		return [];
-	}
-  return students.map(student => student.id);
+const getListStudentIds = (students) => {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.map((student) => student.id);
 };
-export default getListStudentIds; 
-
+export default getListStudentIds;
