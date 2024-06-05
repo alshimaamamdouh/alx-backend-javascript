a3063099beec84b526adfefefb9275f177c5faeb
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-06-05 01:09:29 +0000

    task6

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,15 +2,15 @@ const cleanSet = (set, startString) => {
   if (typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
-  
+
   const result = [];
-  
+
   for (const value of set) {
     if (value.startsWith(startString)) {
       result.push(value.slice(startString.length));
     }
   }
-  
+
   return result.join('-');
 };
-export default cleanSet; 
+export default cleanSet;
