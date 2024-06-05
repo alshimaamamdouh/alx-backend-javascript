09c9b2b6f69b7952ca95f7c61d7234cd92c3aab5
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-06-05 01:08:12 +0000

    task6

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
 const groceriesList = () => {
-  const map = new Map()
-	.set('Apples', 10)
-  	.set('Tomatoes', 10)
- 	.set('Pasta', 1)
-  	.set('Rice', 1)
-  	.set('Banana', 5);
+  const map = new Map();
+  map.set('Apples', 10);
+  map.set('Tomatoes', 10);
+  map.set('Pasta', 1);
+  map.set('Rice', 1);
+  map.set('Banana', 5);
   return map;
 };
-export default groceriesList;
 
+export default groceriesList;
